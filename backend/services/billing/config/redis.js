b763e1171533c76a03f9fa809a20eab02f7a87a1
Jashan Garg <jashangarg961@gmail.com>
2026-07-24 10:37:37 +0530

    commit

---
+++ backend/services/billing/config/redis.js
@@ -0,0 +1,28 @@
+// import Redis from 'ioredis';
+// const redis = new Redis(process.env.REDIS_URL);
+
+// redis.on('connect', () => {
+//   console.log('Redis connected');
+// });
+
+// export default redis;
+
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+import { Redis } from '@upstash/redis';
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+(async () => {
+  try {
+    const pong = await redis.ping();
+    console.log('Redis connected:', pong);
+  } catch (err) {
+    console.error('Redis connection failed:', err);
+  }
+})();
+
+export default redis;
--- backend/shared/redis/redis.js
+++ backend/shared/redis/redis.js
@@ -6,3 +6,23 @@ redis.on('connect', () => {
 });
 
 export default redis;
+
+// import dotenv from 'dotenv';
+// dotenv.config({ path: '../../.env' });
+// import { Redis } from '@upstash/redis';
+
+// const redis = new Redis({
+//   url: process.env.UPSTASH_REDIS_REST_URL,
+//   token: process.env.UPSTASH_REDIS_REST_TOKEN,
+// });
+
+// (async () => {
+//   try {
+//     const pong = await redis.ping();
+//     console.log('Redis connected:', pong);
+//   } catch (err) {
+//     console.error('Redis connection failed:', err);
+//   }
+// })();
+
+// export default redis;
